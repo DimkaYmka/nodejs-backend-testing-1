f613e004aa09c2e99a4fc41bfec22d41b630c602
DimkaYmka <mungalov00@icloud.com>
2025-02-20 12:42:00 +0300

    try

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -2,21 +2,25 @@ import { Post, PostsService } from './posts.service';
 
 describe('PostsService', () => {
   let postsService: PostsService;
-  const post: Omit<Post, 'id' | 'date'> = {
+  const post: omit<Post, 'id' | 'date'> = {
     text: 'Mocked post',
   };
 
   beforeEach(async () => {
     postsService = new PostsService();
-
     postsService.create({ text: 'Some pre-existing post' });
   });
 
   it('should add a new post', () => {
-    // реализуйте тест-кейс
+    const createdPost = postsService.create(post);
+    expect(createdPost).toHaveProperty('id');
+    expect(createdPost).toHaveProperty('date');
+    expect(createdPost.text).toBe(post.text);
   });
 
   it('should find a post', () => {
-    // реализуйте тест-кейс
+    const createdPost = postsService.create({ text: 'Post for finding' });
+    const foundPost = postsService.find(createdPost.id); 
+    expect(foundPost).toEqual(createdPost);
   });
 });
